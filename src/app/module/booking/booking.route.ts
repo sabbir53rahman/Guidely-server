17f3c3f9d460e7c6e292c28c8fa45ee6dc975719
sabbir53rahman <sabbir53rahman@gmail.com>
2026-03-27 10:37:23 +0600

    update booking route

--- src/app/module/booking/booking.route.ts
+++ src/app/module/booking/booking.route.ts
@@ -6,13 +6,37 @@ const router = Router();
 
 // Routes requiring authentication
 router.post("/", checkAuth("STUDENT"), BookingController.createBooking);
-router.get("/me", checkAuth("STUDENT", "MENTOR"), BookingController.getMyBookings);
+router.get(
+  "/me",
+  checkAuth("STUDENT", "MENTOR"),
+  BookingController.getMyBookings,
+);
 
 // General routes (admins or protected differently as needed)
-router.get("/", checkAuth("ADMIN", "SUPER_ADMIN"), BookingController.getAllBookings);
-router.get("/:id", checkAuth("ADMIN", "SUPER_ADMIN", "STUDENT", "MENTOR"), BookingController.getBookingById);
-router.patch("/:id", checkAuth("ADMIN", "SUPER_ADMIN", "MENTOR", "STUDENT"), BookingController.updateBooking);
-router.post("/cancel/:id", checkAuth("ADMIN", "SUPER_ADMIN", "MENTOR", "STUDENT"), BookingController.cancelBooking);
-router.delete("/:id", checkAuth("ADMIN", "SUPER_ADMIN"), BookingController.deleteBooking);
+router.get(
+  "/",
+  checkAuth("ADMIN", "SUPER_ADMIN"),
+  BookingController.getAllBookings,
+);
+router.get(
+  "/:id",
+  checkAuth("ADMIN", "SUPER_ADMIN", "STUDENT", "MENTOR"),
+  BookingController.getBookingById,
+);
+router.patch(
+  "/:id",
+  checkAuth("ADMIN", "SUPER_ADMIN", "MENTOR", "STUDENT"),
+  BookingController.updateBooking,
+);
+router.post(
+  "/cancel/:id",
+  checkAuth("ADMIN", "SUPER_ADMIN", "MENTOR", "STUDENT"),
+  BookingController.cancelBooking,
+);
+router.delete(
+  "/:id",
+  checkAuth("STUDENT", "ADMIN", "SUPER_ADMIN"),
+  BookingController.deleteBooking,
+);
 
 export const BookingRoutes = router;
